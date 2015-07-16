c56e1c7dd1b6711b7ee9c0a2e88ba1be241e4299
Giles MM <giles.williams@gmail.com>
2015-07-16 14:32:22 +0000

    Added berksfile

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,12 @@
 include_recipe 'osrm::install_git'
 
+Chef::Log.info("About to run osrm_map for GB")
+
 osrm_map 'great-britain' do
   action :create_if_missing
 end
+
+Chef::Log.info("About to run osrm_routed for GB")
 
 osrm_routed 'great-britain' do
   user   'osrm-routed'
